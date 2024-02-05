df27f5a372f18af966bfed26581fb2b89817f4b6
tesla <rajieb1618@gmail.com>
2024-02-06 00:13:21 +0600

    initial

---
+++ database/migrations/2024_02_05_180357_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id(); // This creates the 'id' column as the auto-incrementing primary key
+            $table->string('client_name')->notNullable();
+            $table->string('passport_no')->nullable();
+            $table->string('visa_type')->nullable();
+            $table->decimal('amount_paid', 10, 2)->default(0);
+            $table->decimal('amount_due', 10, 2)->default(0);
+            $table->string('status')->default('submitted');
+            $table->string('attachment')->nullable();
+            $table->unsignedBigInteger('agent_id')->nullable();
+            $table->timestamps();
+        
+            // Define foreign key constraint
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -131,6 +131,8 @@
                                     </li>
                                 @endif
 
+                                
+
                                 @if($show_dashboard== 1)
                                         @can('show hrm dashboard')
                                             <li class="dash-item dash-hasmenu">
@@ -232,6 +234,143 @@
                     @endif
 
                     <!--------------------- End Dashboard ----------------------------------->
+
+                    <!--------------------- Start Agents ----------------------------------->
+
+                    @if($show_dashboard == 1)
+                        @if( Gate::check('manage lead') || Gate::check('manage deal') || Gate::check('manage form builder') || Gate::check('manage contract'))
+                            <li class="dash-item dash-hasmenu {{ (Request::segment(1) == 'stages' || Request::segment(1) == 'labels' || Request::segment(1) == 'sources' || Request::segment(1) == 'lead_stages' || Request::segment(1) == 'pipelines' || Request::segment(1) == 'deals' || Request::segment(1) == 'leads'  || Request::segment(1) == 'form_builder' || Request::segment(1) == 'form_response' || Request::segment(1) == 'contract') ?' active dash-trigger':''}}">
+                                <a href="#!" class="dash-link"
+                                ><span class="dash-micon"><i class="ti ti-layers-difference"></i></span
+                                    ><span class="dash-mtext">{{__('Agents')}}</span
+                                    ><span class="dash-arrow"><i data-feather="chevron-right"></i></span
+                                    ></a>
+                                <ul class="dash-submenu {{ (Request::segment(1) == 'stages' || Request::segment(1) == 'labels' || Request::segment(1) == 'sources' || Request::segment(1) == 'lead_stages' || Request::segment(1) == 'leads'  || Request::segment(1) == 'form_builder' || Request::segment(1) == 'form_response' || Request::segment(1) == 'deals' || Request::segment(1) == 'pipelines')?'show':''}}">
+                                    @can('manage lead')
+                                        <li class="dash-item {{ (Request::route()->getName() == 'leads.list' || Request::route()->getName() == 'leads.index' || Request::route()->getName() == 'leads.show') ? ' active' : '' }}">
+                                            <a class="dash-link" href="{{ route('leads.index') }}">{{__('Work Permit Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage deal')
+                                        <li class="dash-item {{ (Request::route()->getName() == 'deals.list' || Request::route()->getName() == 'deals.index' || Request::route()->getName() == 'deals.show') ? ' active' : '' }}">
+                                            <a class="dash-link" href="{{route('deals.index')}}">{{__('Student Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage form builder')
+                                        <li class="dash-item {{ (Request::segment(1) == 'form_builder' || Request::segment(1) == 'form_response')?'active open':''}}">
+                                            <a class="dash-link" href="{{route('form_builder.index')}}">{{__('Business Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @if(\Auth::user()->type=='company' || \Auth::user()->type=='client')
+                                        <li class="dash-item  {{ (Request::segment(1) == 'contract')?'active':''}}">
+                                            <a class="dash-link" href="{{route('contract.index')}}">{{__('Tourist Visa')}}</a>
+                                        </li>
+                                    @endif
+                                    @if(\Auth::user()->type=='company' || \Auth::user()->type=='client')
+                                        <li class="dash-item  {{ (Request::segment(1) == 'contract2')?'active':''}}">
+                                            <a class="dash-link" href="#">{{__('Others')}}</a>
+                                        </li>
+                                    @endif
+                                    
+                                </ul>
+                            </li>
+                        @endif
+                    @endif
+
+                        <!--------------------- End Agents ----------------------------------->
+
+                        <!--------------------- Start Vendors ----------------------------------->
+
+                        @if($show_dashboard == 1)
+                        @if( Gate::check('manage project'))
+                            <li class="dash-item dash-hasmenu {{ ( Request::segment(1) == 'project' || Request::segment(1) == 'bugs-report' || Request::segment(1) == 'bugstatus' || Request::segment(1) == 'project-task-stages' || Request::segment(1) == 'calendar' || Request::segment(1) == 'timesheet-list' || Request::segment(1) == 'taskboard' || Request::segment(1) == 'timesheet-list' || Request::segment(1) == 'taskboard' || Request::segment(1) == 'project' || Request::segment(1) == 'projects' || Request::segment(1) == 'project_report')
+                            ? 'active dash-trigger' : ''}}">
+                                <a href="#!" class="dash-link"
+                                ><span class="dash-micon"><i class="ti ti-share"></i></span
+                                    ><span class="dash-mtext">{{__('Vendors')}}</span
+                                    ><span class="dash-arrow"><i data-feather="chevron-right"></i></span
+                                    ></a>
+                                <ul class="dash-submenu">
+                                    @can('manage project')
+                                        <li class="dash-item  {{Request::segment(1) == 'project' || Request::route()->getName() == 'projects.list' || Request::route()->getName() == 'projects.list' ||Request::route()->getName() == 'projects.index' || Request::route()->getName() == 'projects.show' || request()->is('projects/*') ? 'active' : ''}}">
+                                            <a class="dash-link" href="{{route('projects.index')}}">{{__('Work Permit Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage project task')
+                                        <li class="dash-item {{ (request()->is('taskboard*') ? 'active' : '')}}">
+                                            <a class="dash-link" href="{{ route('taskBoard.view', 'list') }}">{{__('Student Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage timesheet')
+                                        <li class="dash-item {{ (request()->is('timesheet-list*') ? 'active' : '')}}">
+                                            <a class="dash-link" href="{{route('timesheet.list')}}">{{__('Business Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage bug report')
+                                        <li class="dash-item {{ (request()->is('bugs-report*') ? 'active' : '')}}">
+                                            <a class="dash-link" href="{{route('bugs.view','list')}}">{{__('Tourist Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage project task')
+                                        <li class="dash-item {{ (request()->is('calendar*') ? 'active' : '')}}">
+                                            <a class="dash-link" href="{{ route('task.calendar',['all']) }}">{{__('Others')}}</a>
+                                        </li>
+                                    @endcan
+                                    
+                                    @if(Gate::check('manage project task stage') || Gate::check('manage bug status'))
+                                       
+                                    @endif
+                                </ul>
+                            </li>
+                        @endif
+                    @endif
+
+                        <!--------------------- End Vendors ----------------------------------->
+
+
+
+                       
+                       
+                        <!--------------------- Start Clients ----------------------------------->
+
+                        @if( Gate::check('manage warehouse') ||  Gate::check('manage purchase')  || Gate::check('manage pos') || Gate::check('manage print settings'))
+                            <li class="dash-item dash-hasmenu {{ (Request::segment(1) == 'warehouse' || Request::segment(1) == 'purchase' || Request::route()->getName() == 'pos.barcode' || Request::route()->getName() == 'pos.print' || Request::route()->getName() == 'pos.show')?' active dash-trigger':''}}">
+                                <a href="#!" class="dash-link"><span class="dash-micon"><i class="ti ti-layers-difference"></i></span><span class="dash-mtext">{{__('Clients')}}</span><span class="dash-arrow"><i data-feather="chevron-right"></i></span></a>
+                                <ul class="dash-submenu {{ (Request::segment(1) == 'warehouse' || Request::segment(1) == 'purchase' || Request::route()->getName() == 'pos.barcode' || Request::route()->getName() == 'pos.print' || Request::route()->getName() == 'pos.show')?'show':''}}">
+                                    @can('manage warehouse')
+                                        <li class="dash-item {{ (Request::route()->getName() == 'warehouse.index' || Request::route()->getName() == 'warehouse.show') ? ' active' : '' }}"><a class="dash-link" href="{{ route('warehouse.index') }}">{{__('Work Permit Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage purchase')
+                                        <li class="dash-item {{ (Request::route()->getName() == 'purchase.index' || Request::route()->getName() == 'purchase.create' || Request::route()->getName() == 'purchase.edit' || Request::route()->getName() == 'purchase.show') ? ' active' : '' }}">
+                                            <a class="dash-link" href="{{ route('purchase.index') }}">{{__('Student Visa')}}</a>
+                                        </li>
+                                    @endcan
+                                    @can('manage quotation')
+                                    <li
+                                        class="dash-item {{ Request::route()->getName() == 'quotation.index' || Request::route()->getName() == 'quotations.create' || Request::route()->getName() == 'quotation.edit' || Request::route()->getName() == 'quotation.show' ? ' active' : '' }}">
+                                        <a class="dash-link" href="{{ route('quotation.index') }}">{{ __('Business Visa') }}</a>
+                                    </li>
+                                @endcan
+                                    @can('manage pos')
+                                        <li class="dash-item {{ (Request::route()->getName() == 'pos.index' ) ? ' active' : '' }}">
+                                            <a class="dash-link" href="{{ route('pos.index') }}">{{__(' Tourist Visa')}}</a>
+                                        </li>
+
+                                        <li class="dash-item {{ (Request::route()->getName() == 'pos.report' || Request::route()->getName() == 'pos.show') ? ' active' : '' }}">
+                                            <a class="dash-link" href="{{ route('pos.report') }}">{{__('Others')}}</a>
+                                        </li>
+                                    @endcan
+                                       
+                                    
+                                    
+
+                                </ul>
+                            </li>
+                        @endif
+
+
+                        <!--------------------- End Clients ----------------------------------->
 
 
                     <!--------------------- Start HRM ----------------------------------->
